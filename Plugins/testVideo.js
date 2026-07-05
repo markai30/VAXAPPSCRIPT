c6c042f82d5187e8cc6dad83d0d12a389c06949e
markai30 <matran999@gmail.com>
2026-07-05 18:42:11 +0700

    Update testVideo.js

--- Plugins/testVideo.js
+++ Plugins/testVideo.js
@@ -10,7 +10,7 @@ function getManifest() {
         "id": "testvideo",          
         "name": "Test Embed",
         "description": "Nguồn xem phim Online ổn định",
-        "version": "1.3",             
+        "version": "1.4",             
         "baseUrl": BaseURL,
         "iconUrl": "https://crimescenesolutions.co.za/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
@@ -139,7 +139,7 @@ function parseDetailResponse(html,url) {
         var refUrl = BaseJSON.ref || "";
         var agent = BaseJSON.codeb || "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
         url = JSON.stringify(url);
-        var allLink =  getAllLinks(html);
+        var allLink =  JSON.stringify(getAllLinks(html));
         var customjs = BaseJSON.codec || "";
         customjs += `
         function runScript(){
@@ -183,21 +183,14 @@ function parseDetailResponse(html,url) {
 
 function getAllLinks(html) {
   // Lấy toàn bộ HTML của trang hiện tạ
-  const linkRegex = /(https?:\/\/[^\s"'<>]+|(?<![\w/])[a-zA-Z0-9.-]+\.(?:com|net|org|edu|gov|mil|biz|info|vn|me|io)[^\s"'<>]*)/gi;
-
-  // Tìm tất cả các chuỗi khớp với regex
+ const linkRegex = /(https?:\/\/[^\s"'<>]+|(?<![\w/])[a-zA-Z0-9.-]+\.(?:com|net|org|edu|gov|mil|biz|info|vn|me|io)[^\s"'<>]*)/gi;
   const matches = html.match(linkRegex);
 
-  // return này hợp lệ vì đã nằm TRONG hàm
-  if (!matches) return ""; 
+  if (!matches) {
+    return "";
+  }
 
-  // Loại bỏ các link bị trùng lặp
-  const uniqueLinks = [...new Set(matches)];
-  
-  // Gộp lại bằng dấu xuống dòng (bạn nhớ đổi '##@' thành '\n' nhé)
-  var allLink = uniqueLinks.join('@@');
-  
-  return allLink;
+  return Array.from(new Set(matches)).join('\n');
 }
 
 
