c7f2d3d834328bc7db6f98dcd397f7962f90ccec
markai30 <matran999@gmail.com>
2026-07-05 23:35:52 +0700

    Update testVideo.js

--- Plugins/testVideo.js
+++ Plugins/testVideo.js
@@ -11,7 +11,7 @@ function getManifest() {
         "id": "testvideo",          
         "name": "Test Embed",
         "description": "Nguồn xem phim Online ổn định",
-        "version": "1.11",             
+        "version": "1.21",             
         "baseUrl": BaseURL,
         "iconUrl": "https://crimescenesolutions.co.za/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
@@ -139,19 +139,10 @@ function parseDetailResponse(html,url) {
         var videoUrl = BaseJSON.link || "";
         var refUrl = BaseJSON.ref || "";
         var agent = BaseJSON.codeb || "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
-        url = JSON.stringify(url);
-        var joinnew = videoUrl;
-        if(BaseURL){
-            joinnew += BaseURL;
-        }
-        
-        var allLink =  base64Encode(html);
         var customjs = BaseJSON.codec || "";
         customjs += `
         function runScript($title,$msg){
-            var urlList = '${allLink}';
-            var decodehtml = decodeBase64ToHtml($urlList);
-            customAlert($msg + ':${url}', decodehtml);
+            customAlert($msg + $title, $msg);
         }
         function decodeBase64ToHtml(base64String) {
             const binaryString = atob(base64String);
